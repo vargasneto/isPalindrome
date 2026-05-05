a055d881035d524b3f5359b5bb898ab86be12ffb
Pedro <pedrohjs_vargas@hotmail.com>
2026-05-05 00:14:51 -0300

    chore:add for e melhores estruturas / palindromes é uma lista agora (verifica mais de um valor)

--- palindrome.py
+++ palindrome.py
@@ -3,9 +3,19 @@ def is_palindrome(word):
     return word == word[::-1]
 
 
-text = input("Digite uma palavra: ")
+text = input("Digite duas ou mais palavras: ")
+words = text.split()
+palindromes = []
 
-if is_palindrome(text):
-    print("Is Palindrome!")
+for word in words:
+    if is_palindrome(word):
+        palindromes.append(word)
+
+if not words:
+    print("Nenhuma palavra informada")
+elif palindromes:
+    print("Palindromos encontrados:")
+    for word in palindromes:
+        print(word)
 else:
-    print("Not is palindrome")
+    print("Nenhuma palavra e palindromo")
